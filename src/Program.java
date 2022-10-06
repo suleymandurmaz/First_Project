f7dc47ff650477ca8ab1fcd3a6bbf4666bedb3bb
suleymandurmaz <sdurmaz77@hotmail.com>
2022-10-06 19:52:47 +0300

    firs proje created

---
+++ src/Program.java
@@ -0,0 +1,22 @@
+
+
+public class Program {
+    public static void main(String[]args){
+/*        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");*/
+
+     /*   System.out.println("*\t*\t*\t*");
+        System.out.println("*\t*\t*\t*");
+        System.out.println("*\t*\t*\t*");
+        System.out.println("*\t*\t*\t*");*/
+        //task:Yıldızlardan olusan bir kare cıktısı veren java programı yazın
+/*        System.out.print("merhaba");
+        System.out.println(" kardes");*/
+        System.out.println("    *");
+        System.out.println("  *   *");
+        System.out.println(" *     *");
+        System.out.println("*       *");
+        System.out.println("*********");
+    }
+}
---
+++ src/kare.java
@@ -0,0 +1,19 @@
+public class kare {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 54; i++) {
+
+
+                if(54%12==0){
+                    System.out.print("*");
+
+            }
+
+
+            }
+
+        }
+
+
+    }
+
+
